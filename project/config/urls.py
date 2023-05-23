edddf5f7bdea9b76ef599c2b926f8c4021a8f8dd
tincho <lagrotteriamartin0@gmail.com>
2023-05-22 21:57:48 -0300

    Primer Commit

---
+++ project/config/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include(("home.urls", "home"))),
+     path("", include(("venta.urls", "venta"))),
+    path("producto/", include(("producto.urls", "producto")))
+]
+
